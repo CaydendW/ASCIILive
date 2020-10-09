ad3ef3ab7522082267edda6cf96512ce09d5a1af
Cayden de Wit <cayden@4dewits.com>
2020-10-09 14:42:21 +0200

    Fixed many issues

--- ASCIILive.py
+++ ASCIILive.py
@@ -12,8 +12,14 @@ print("Made by: CaydendW")
 print("Note: You may have to resize terminal window to get best effect.")
 print("Note: This does not work for Linux.")
 print("Note: Press control+c in the terminanl or q in the window to exit. (crtl+c) (q)")
-new_width = int(input("How big do you want the image?: "))
-theme = str(input("Press d for dark mode and leave blank for light: "))
+
+try:
+    new_width = int(input("How big do you want the image?: "))
+except:
+    print(new_width + " is not a valid int")
+
+theme = str(input("Press d for dark mode and l for light mode (This depends on your terminal background): "))
+pos = input("Please put w for windows or l for linux: ").lower()
 
 chars = []
 cap = cv2.VideoCapture(0)
@@ -28,49 +34,59 @@ def disable_stdout_buffering():
 
 if theme=="d":
     chars = [" ",":","!","*","%","$","@","&","#","S","B"]
-else:
+elif theme=="d":
     chars = ["B","S","#","&","@","$","%","*","!",":"," "]
+else:
+    print(theme + " is not a valid option")
+    exit()
 
-while(True):
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.flip(gray, 1)
-    cv2.imshow('frame',gray)
+if pos=="w":
+    lspsa="cls"
+elif pos=="l":
+    lspsa="clear"
+else:
+    print(pos + " is not a valid option")
+    exit()
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+try:
+    while(True):
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.flip(gray, 1)
+        cv2.imshow('frame',gray)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+            os.system('rm -rf Python.jpg')
+            os.system(lspsa)
+            cap.release()
+            cv2.destroyAllWindows()
+            exit()
+
+        cv2.imwrite(r"Python.jpg",gray)
+        image_path = r"Python.jpg"
+        img = Image.open(image_path)
+
+        os.system(lspsa)
         
-    try:
-        a = "B"
-    except KeyboardInterrupt:
-        break
+        width, height = img.size
+        aspect_ratio = height/width
+        new_height = aspect_ratio * new_width * 0.55
+        img = img.resize((new_width, int(new_height)))
+        img = img.convert('L')
+        pixels = img.getdata()
+        new_pixels = [chars[pixel//25] for pixel in pixels]
+        new_pixels = ''.join(new_pixels)
+        new_pixels_count = len(new_pixels)
+        ascii_image = [new_pixels[index:index + new_width] for index in range(0, new_pixels_count, new_width)]
+        ascii_image = "\n".join(ascii_image)
+        
+        disable_stdout_buffering()
+        print(ascii_image)
 
-    cv2.imwrite(r"D:\Users\cayde\Documents\Code\Resources\Python.jpg",gray)
-    image_path = r"D:\Users\cayde\Documents\Code\Resources\Python.jpg"
-    img = Image.open(image_path)
-
-    os.system('cls')
-    
-    width, height = img.size
-    aspect_ratio = height/width
-    new_height = aspect_ratio * new_width * 0.55
-    img = img.resize((new_width, int(new_height)))
-    img = img.convert('L')
-    pixels = img.getdata()
-    new_pixels = [chars[pixel//25] for pixel in pixels]
-    new_pixels = ''.join(new_pixels)
-    new_pixels_count = len(new_pixels)
-    ascii_image = [new_pixels[index:index + new_width] for index in range(0, new_pixels_count, new_width)]
-    ascii_image = "\n".join(ascii_image)
-    
-    disable_stdout_buffering()
-    print(ascii_image)
-
-    try:
-        a = "B"
-    except KeyboardInterrupt:
-        break
-
-os.system('cls')
-cap.release()
-cv2.destroyAllWindows()
+except KeyboardInterrupt:
+    os.system('rm -rf Python.jpg')
+    cap.release()
+    os.system(lspsa)
+    cv2.destroyAllWindows()
+    exit()
